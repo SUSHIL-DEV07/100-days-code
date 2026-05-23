05c58d5062f24a1ff5f0878f7be37b2e0fcd78ea
SUSHIL-DEV07 <zangadesushil13@gmail.com>
2026-05-23 15:55:06 +0530

    Day 1 Complete

---
+++ Day1.py
@@ -0,0 +1,178 @@
+# Basic 
+print ("My Name is Sushul " , "Age : 20")
+print ("Hellow world !! ")
+print (23)
+
+
+# Character set
+
+# Engilsh A - Z 
+# Digit 0 - 9 
+# Special Symbol 
+# whitespace 
+# other character 
+
+
+# Varible 
+
+age = 20 
+mark = 19.3
+name = "Sushil"
+
+
+name1 = "Same"
+name2 = 'Same'
+name3 = '''Same'''
+
+
+
+# Rules Of Varible  
+
+
+# Datatype 
+
+A = 10 
+B = 10.02
+C = "String"
+D = False 
+E = None 
+
+F = 10 + 5j
+print (type (F))
+
+print (f"   A =  {A}")
+
+
+# Keywords 
+
+
+
+# Problem  
+
+a = 10 
+b = 20 
+
+c = a + b 
+
+print (c) 
+
+
+
+
+
+
+# Comment
+
+
+# single
+
+'''
+multi
+line 
+comment 
+'''
+
+
+
+
+# Operator 
+
+# # Arithmetic  
+
+# print (5+5)
+# print (5-5)
+# print (5*5)
+# print (5/5 )
+# print (5%5)
+# print (5**2) # Power
+# print (3 // 2)
+
+# relational / comparasion 
+
+# print (1<2)
+# print (1>3)
+# print (1 == 2)
+# print (1 != 2)
+# print (2 >= 1)
+# print (1 <= 2 )
+
+# Assissiment 
+
+a = 10 
+a += 10 
+a -= 10 
+a *= 10 
+a /= 10 
+a %= 2  
+a **= 2 
+
+# Logical
+
+print (not True )
+print (True and False )
+print (True or False)
+
+
+# Type Conversion
+
+# Automatioc
+a = 10 
+b = 10.10 
+
+print (a + b)
+
+
+
+# Manual
+a = 10 
+b = int("20")
+print (a+b)
+
+
+
+# input 
+
+# a = input ("Enter Your Name : ")
+# print (a)
+
+# num = int ( input ("Enter Number : "))
+# float = float (input ("Enter Float : "))
+# bool = bool (input ("Enter Boolean : "))
+
+# print (num , float , bool)
+
+
+
+
+# practice Question 1 
+
+# a = int (input ("First Number : "))
+# b = int (input ("Second Numer : "))
+
+# print (a + b )
+
+
+# practice Question 2 
+
+# side = float (input ("Enter Square side : "))
+# Area = side * side 
+
+# print (Area)
+
+
+# practice Question 3
+
+# first = int (input ("Enter Num 1 : ")) 
+# second = int (input ("Enter Num 2 : "))
+
+# Avg = (first + second) / 2
+
+# print (Avg)
+
+
+# practice Question 4
+
+A = int ( input ( "Enter A : " ) )
+B = int ( input ( "Enter B : " ) )
+
+print (A >= B)
